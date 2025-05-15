85222f32672c82694b3853eb9c631c25061f3ca4
BatMaxou <maximebatista.lycee@gmail.com>
2025-05-15 23:39:38 +0200

    :recycle: Move buildFromInterface to TagBuilder

--- src/Interface/TagBuilderInterface.php
+++ src/Interface/TagBuilderInterface.php
@@ -13,4 +13,19 @@ interface TagBuilderInterface
      * @return T|string
      */
     public function buildFromName(string $name, array $options = []): TagInterface|string;
+
+    /**
+     * @param \BackedEnum[] $options
+     *
+     * @return T|string
+     */
+    public function buildFromInterface(string $interface, $options = []): TagInterface|string;
+
+    /**
+     * @param class-string[] $interfaces
+     * @param \BackedEnum[] $options
+     *
+     * @return TagInterface[]|string[]
+     */
+    public function buildFromInterfaces(array $interfaces, $options = []): array;
 }
--- src/Service/TagBuilder.php
+++ src/Service/TagBuilder.php
@@ -14,6 +14,33 @@ use Aatis\Tag\Interface\TagInterface;
  */
 class TagBuilder implements TagBuilderInterface
 {
+    public function buildFromInterfaces(array $interfaces, $options = []): array
+    {
+        $tags = [];
+
+        foreach ($interfaces as $interface) {
+            $tags[] = $this->buildFromInterface($interface, $options);
+        }
+
+        return array_unique($tags);
+    }
+
+    public function buildFromInterface(string $interface, $options = []): TagInterface|string
+    {
+        $explode = explode('\\', $interface);
+        $interface = $explode[count($explode) - 1];
+        if (str_ends_with($interface, 'Interface')) {
+            $interface = substr($interface, 0, -9);
+        }
+
+        $tag = implode(array_map(
+            fn ($letter) => ctype_upper($letter) ? sprintf('-%s', strtolower($letter)) : $letter,
+            str_split(lcfirst($interface)),
+        ));
+
+        return $this->buildFromName($tag, $options);
+    }
+
     public function buildFromName(string $name, array $options = []): TagInterface|string
     {
         $tag = $this->newTag($options)->setName($name);
